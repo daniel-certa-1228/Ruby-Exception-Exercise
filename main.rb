4887d6dd81614a53944d28c8c492345a5ada4273
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-10 11:08:18 -0500

    fixed error handling in the CALL

--- main.rb
+++ main.rb
@@ -1,14 +1,16 @@
 require_relative 'patient'
 
-# takes care of puts patient_contact_info
-def method_missing(method_name)
-	puts "There is no method called #{method_name}"
-end
-
 new_patient = Patient.new("Jisie", "David")
 puts new_patient.first_name
 puts new_patient.last_name
+
+#Handle exception at the CALL
+begin
 puts patient_contact_info
+rescue NameError
+	puts "There is no fucntion by that name"
+end	
+
 puts new_patient.patient_status(:coma)
 
 # Could only get an handling by rescuing at the CALL rather than in the function
@@ -21,3 +23,8 @@ end
 puts new_patient.patient_new_total(125)
 new_patient.add_med_to_list("lipitor", 123.12)
 new_patient.patient_medications_list
+
+# takes care of puts patient_contact_info
+# def method_missing(method_name)
+# 	puts "There is no method called #{method_name}"
+# end
